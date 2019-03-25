47ed81fc0e48eca7d0e02e32f52a95288e19b29f
El bouslamti Omar <elbouslamti.omar@gmail.com>
2019-03-25 20:28:48 +0100

    les composant nécesssaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompoComponent } from './compo/compo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompoComponent
   ],
   imports: [
     BrowserModule,
